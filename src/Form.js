57a15d3b8522a18280d1bcb793e04e4ad47c8981
wzamites <bj_zamites@comcast.net>
2020-08-26 02:33:57 -0400

    there's no need for a class component here

--- src/Form.js
+++ src/Form.js
@@ -97,18 +97,12 @@ function StateDropdown(props) {
   )
 }
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
+function Form(props) {
+  function handleSubmit(event) {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const city = this.props.city;
-    const state = this.props.state;
-    const zipcode = this.props.zipcode;
+    const city = props.city;
+    const state = props.state;
+    const zipcode = props.zipcode;
     const url = `https://www.zipcodeapi.com/rest/N4YTIo3lM8IXkucKBBOKQz3DpJj7yb0WCxSqjPomtRzLGa7tOfJe1fmgMfeod5u0/city-zips.json/${city}/${state}`
     fetch(proxyurl + url)
     .then(response => {return response.json()})
@@ -116,7 +110,7 @@ class Form extends React.Component {
     .then(data => data.includes(zipcode))
     .then(isZipcode => {
       if (isZipcode) {
-        this.props.history.push('./success');
+        props.history.push('./success');
       } else {
         alert("The Zip isn't valid")
       }
@@ -124,50 +118,48 @@ class Form extends React.Component {
     event.preventDefault();
   }
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <Row>
-            <Col>
-              <Field type='text' label='Name' name='name' placeholder="John Doe" value={this.props.name} onChange={this.props.onChange} className='fullWidth' required='required'/>
-              <br/>
-              <br/>
-              <Field type='text' label='Address Line 1' name='street' placeholder="123 Main St." value={this.props.street} onChange={this.props.onChange} className='fullWidth' required='required'/>
-              <br/>
-              <br/>
-              <Field type='text' label='Address Line 2' name='street2' placeholder="Apt 5A (optional)" value={this.props.street2} onChange={this.props.onChange} className='fullWidth'/>
-              <br/>
-              <br/>
-              <Field type='text' label='City' name='city' placeholder="Los Angeles" value={this.props.city} onChange={this.props.onChange} className='fullWidth' required='required'/>
-              <br />
-              <br/>
-              <StateDropdown value={this.props.state} onChange={this.props.onChange} className='fullWidth' required='required' />
-              <br/>
-              <br/>
-              <Field type='text' label='Zip' name='zipcode' placeholder="90001" value={this.props.zipcode} onChange={this.props.onChange} className='fullWidth' required='required'/>
-              <br/>
-              <br/>
-              <Field type='text' label="Email" name="email" placeholder="john@hometap.com" value={this.props.email} onChange={this.props.onChange} className='fullWidth' required='required'/>
-              <br/>
-              <br/>
-              <Field type='text' label="Phone Number" name="phone" placeholder="973-234-2353" value={this.props.phone} onChange={this.props.onChange} className='fullWidth' required='required'/>
-              <br/>
-              <br/>
-            </Col>
-            <Col>
-              <Checkbox label="Product A" name="isProductA" checked={this.props.isProductA} onChange={this.props.onChange}/>
-              <Checkbox label="Product B" name="isProductB" checked={this.props.isProductB} onChange={this.props.onChange}/>
-              <Checkbox label="Product C" name="isProductC" checked={this.props.isProductC} onChange={this.props.onChange}/>
-            </Col>
-          </Row>
-          <Row>
-            <input type="submit" value="Submit"/>
-          </Row>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Row>
+          <Col>
+            <Field type='text' label='Name' name='name' placeholder="John Doe" value={props.name} onChange={props.onChange} className='fullWidth' required='required'/>
+            <br/>
+            <br/>
+            <Field type='text' label='Address Line 1' name='street' placeholder="123 Main St." value={props.street} onChange={props.onChange} className='fullWidth' required='required'/>
+            <br/>
+            <br/>
+            <Field type='text' label='Address Line 2' name='street2' placeholder="Apt 5A (optional)" value={props.street2} onChange={props.onChange} className='fullWidth'/>
+            <br/>
+            <br/>
+            <Field type='text' label='City' name='city' placeholder="Los Angeles" value={props.city} onChange={props.onChange} className='fullWidth' required='required'/>
+            <br />
+            <br/>
+            <StateDropdown value={props.state} onChange={props.onChange} className='fullWidth' required='required' />
+            <br/>
+            <br/>
+            <Field type='text' label='Zip' name='zipcode' placeholder="90001" value={props.zipcode} onChange={props.onChange} className='fullWidth' required='required'/>
+            <br/>
+            <br/>
+            <Field type='text' label="Email" name="email" placeholder="john@hometap.com" value={props.email} onChange={props.onChange} className='fullWidth' required='required'/>
+            <br/>
+            <br/>
+            <Field type='text' label="Phone Number" name="phone" placeholder="973-234-2353" value={props.phone} onChange={props.onChange} className='fullWidth' required='required'/>
+            <br/>
+            <br/>
+          </Col>
+          <Col>
+            <Checkbox label="Product A" name="isProductA" checked={props.isProductA} onChange={props.onChange}/>
+            <Checkbox label="Product B" name="isProductB" checked={props.isProductB} onChange={props.onChange}/>
+            <Checkbox label="Product C" name="isProductC" checked={props.isProductC} onChange={props.onChange}/>
+          </Col>
+        </Row>
+        <Row>
+          <input type="submit" value="Submit"/>
+        </Row>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
